3f556a01d1dc27758e3f6f7e5f6139ee88018e48
Eddie Mishelevich <eddie@everything.me>
2015-03-23 20:40:24 +0200

    Skeleton of an example camera app

---
+++ android_app/camera/src/main/java/com/ultimate/camera/activities/MainActivity.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014 Rex St. John on behalf of AirPair.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.ultimate.camera.activities;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ultimate.camera.R;
+import com.ultimate.camera.fragments.BaseFragment;
+import com.ultimate.camera.fragments.NativeCameraFragment;
+
+/**
+ * Created by Rex St. John (on behalf of AirPair.com) on 3/4/14.
+ */
+public class MainActivity extends CameraActivity {
+
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = getString(R.string.title_section4);
+        FragmentManager fragmentManager = getFragmentManager();
+
+        BaseFragment targetFragment = NativeCameraFragment.newInstance(5);
+
+        fragmentManager.beginTransaction().replace(R.id.container, targetFragment).commit();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+}
