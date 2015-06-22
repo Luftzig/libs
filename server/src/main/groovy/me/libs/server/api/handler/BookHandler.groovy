5997b0c31049cd984bb8bd32a5e6481f2362b75e
noamt <noam@10ne.org>
2015-06-22 15:21:32 +0300

    Test the book handler

--- server/src/main/groovy/me/libs/server/api/handler/BookHandler.groovy
+++ server/src/main/groovy/me/libs/server/api/handler/BookHandler.groovy
@@ -1,37 +1,85 @@
 package me.libs.server.api.handler
 
-import ratpack.handling.Context
-import ratpack.handling.Handler
-import ratpack.http.HttpMethod
-import ratpack.http.Request
-import ratpack.http.Response
-import ratpack.path.PathTokens
+import ratpack.groovy.handling.GroovyContext
+import ratpack.groovy.handling.GroovyHandler
+
+import static io.netty.handler.codec.http.HttpResponseStatus.*
+import static ratpack.http.internal.HttpHeaderConstants.JSON
 
 /**
  * @author Noam Y. Tenne
  */
-class BookHandler implements Handler {
+class BookHandler extends GroovyHandler {
 
     @Override
-    void handle(Context context) throws Exception {
-        def response = context.response;
-        def pathTokens = context.pathTokens;
-
-        def request = context.request;
-        def method = request.method;
-        if (pathTokens.containsKey('id')) {
-            if (method.get) {
-                response.contentType('application/json').send('{ "id": "id", "authors": ["Jim Koogleshreiber", "John Boochmacher"], ' +
-                        '"title": "Necronomicon", "isbn (optional)": "978-0380751921", "cover": "link/binary" }');
-            } else if (method.post) {
-                response.send("Book ${pathTokens.get('id')} has been updated.");
-            } else if (method.delete) {
-                response.send("Book ${pathTokens.get('id')}  has been removed.");
+    void handle(GroovyContext context) {
+        context.byMethod {
+            get {
+                bookById(context)
             }
-        } else {
-            if (method.put) {
-                response.status(201).contentType('application/json').send('{ "id": "id" }');
+            put {
+                addBook(context)
+            }
+            post {
+                updateBook(context)
+            }
+            delete {
+                deleteBook(context)
             }
         }
+    }
+
+    private void bookById(GroovyContext groovyContext) {
+        if (noIdSpecified(groovyContext)) {
+            missingId(groovyContext)
+            return
+        }
+        groovyContext.response.contentType(JSON).status(OK)
+                .send("{ \"id\": \"${groovyContext.pathTokens.id}\", \"authors\": [\"Jim Koogleshreiber\", \"John Boochmacher\"], " +
+                "\"title\": \"Necronomicon\", \"isbn\": \"978-0380751921\", \"cover\": \"link/binary\" }")
+    }
+
+    private void addBook(GroovyContext groovyContext) {
+        if (contentIsntJson(groovyContext)) {
+            wrongContent(groovyContext)
+            return
+        }
+        groovyContext.response.status(CREATED).contentType(JSON).send('{ "id": "id" }')
+    }
+
+    private void updateBook(GroovyContext groovyContext) {
+        if (noIdSpecified(groovyContext)) {
+            missingId(groovyContext)
+            return
+        }
+        if (contentIsntJson(groovyContext)) {
+            wrongContent(groovyContext)
+            return
+        }
+        groovyContext.response.status(ACCEPTED).send()
+    }
+
+    private void deleteBook(GroovyContext groovyContext) {
+        if (noIdSpecified(groovyContext)) {
+            missingId(groovyContext)
+            return
+        }
+        groovyContext.response.status(ACCEPTED).send()
+    }
+
+    private boolean noIdSpecified(GroovyContext groovyContext) {
+        !groovyContext.pathTokens.containsKey('id')
+    }
+
+    private void missingId(GroovyContext groovyContext) {
+        groovyContext.response.status(BAD_REQUEST).contentType(JSON).send("{\"errors:\": [\"This command requires a book ID\"]}")
+    }
+
+    private boolean contentIsntJson(GroovyContext groovyContext) {
+        !groovyContext.request.body.contentType.json
+    }
+
+    private void wrongContent(GroovyContext groovyContext) {
+        groovyContext.response.status(BAD_REQUEST).contentType(JSON).send("{\"errors:\": [\"This command requires a JSON body\"]}")
     }
 }
---
+++ server/src/test/groovy/me/libs/server/api/handler/BookHandlerSpec.groovy
@@ -0,0 +1,115 @@
+package me.libs.server.api.handler
+
+import groovy.json.JsonSlurper
+import ratpack.groovy.test.handling.internal.DefaultGroovyRequestFixture
+import ratpack.http.internal.HttpHeaderConstants
+import spock.lang.Specification
+
+import static io.netty.handler.codec.http.HttpResponseStatus.*
+
+/**
+ * @author Noam Y. Tenne
+ */
+class BookHandlerSpec extends Specification {
+
+    def 'Get a book by it\'s ID'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            pathBinding(id: '123')
+        }
+
+        expect:
+        result.status.code == OK.code()
+        def resultBody = new JsonSlurper().parse(result.bodyBytes)
+        resultBody.id == '123'
+        resultBody.authors == ['Jim Koogleshreiber', 'John Boochmacher']
+        resultBody.title == 'Necronomicon'
+        resultBody.isbn == '978-0380751921'
+        resultBody.cover == 'link/binary'
+    }
+
+    def 'Get a book but specify no ID'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {}
+
+        expect:
+        result.status.code == BAD_REQUEST.code()
+    }
+
+    def 'Create a new book'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'PUT'
+            body('{}', HttpHeaderConstants.JSON.toString())
+        }
+
+        expect:
+        result.status.code == CREATED.code()
+        def resultBody = new JsonSlurper().parse(result.bodyBytes)
+        resultBody.id == 'id'
+    }
+
+    def 'Create a new book but send no content'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'PUT'
+        }
+
+        expect:
+        result.status.code == BAD_REQUEST.code()
+    }
+
+    def 'Update an existing book'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'POST'
+            pathBinding(id: '123')
+            body('{}', HttpHeaderConstants.JSON.toString())
+        }
+
+        expect:
+        result.status.code == ACCEPTED.code()
+    }
+
+    def 'Update an existing book with no ID'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'POST'
+        }
+
+        expect:
+        result.status.code == BAD_REQUEST.code()
+    }
+
+    def 'Update an existing book with no content'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'POST'
+            pathBinding(id: '123')
+        }
+
+        expect:
+        result.status.code == BAD_REQUEST.code()
+    }
+
+    def 'Delete an existing book'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'DELETE'
+            pathBinding(id: '123')
+        }
+
+        expect:
+        result.status.code == ACCEPTED.code()
+    }
+
+    def 'Delete an existing book with no ID'() {
+        setup:
+        def result = DefaultGroovyRequestFixture.handle(new BookHandler()) {
+            method 'DELETE'
+        }
+
+        expect:
+        result.status.code == BAD_REQUEST.code()
+    }
+}
